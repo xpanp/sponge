77f37e9072e77144dd008f18854bf6b17b2c814b
xpanp <852290039@qq.com>
2022-12-12 11:36:31 +0800

    add lab2 ans

--- libsponge/tcp_receiver.cc
+++ libsponge/tcp_receiver.cc
@@ -11,9 +11,44 @@ void DUMMY_CODE(Targs &&... /* unused */) {}
 using namespace std;
 
 void TCPReceiver::segment_received(const TCPSegment &seg) {
-    DUMMY_CODE(seg);
+    TCPHeader header = seg.header();
+    // 忽略收到SYN之前的数据
+    if (!_syn_received && !header.syn) {
+        return;
+    }
+    // 忽略重复的SYN
+    if (_syn_received && header.syn) {
+        return;
+    }
+
+    string payload = seg.payload().copy();
+    WrappingInt32 seqno = header.seqno;
+    // 收到 SYN
+    if (header.syn) {
+        _syn_received = true;
+        _isn = header.seqno;
+        seqno = header.seqno + 1;
+    }
+    // 收到 FIN
+    if (header.fin) {
+        _fin_received = true;
+    }
+
+    uint64_t checkpoint = _reassembler.stream_out().bytes_written();
+    uint64_t absolute_seqno = unwrap(seqno, _isn, checkpoint);
+    // stream_index = absolute_seqno - 1
+    _reassembler.push_substring(payload, absolute_seqno - 1, header.fin);
 }
 
-optional<WrappingInt32> TCPReceiver::ackno() const { return {}; }
+optional<WrappingInt32> TCPReceiver::ackno() const {
+    if (!_syn_received) {
+        return nullopt;
+    }
+    uint64_t absolute_seqno = stream_out().bytes_written() + 1;
+    stream_out().input_ended() ? ++absolute_seqno : absolute_seqno;
+    return wrap(absolute_seqno, _isn);
+}
 
-size_t TCPReceiver::window_size() const { return {}; }
+size_t TCPReceiver::window_size() const {
+    return stream_out().remaining_capacity();
+}
--- writeups/lab2.md
+++ writeups/lab2.md
@@ -5,3 +5,7 @@ Lab 2 Writeup
 ```bash
 sudo apt install libpcap-dev
 ```
+
+注意看文档，理解seqno、absolute seqno、stream index的关系。
+
+可能之前有丢包或者延迟了，因此收到FIN之后，并没有真正结束，还需要继续等待，直到所有包都组装完成。
